31536e3751e48ccca0150013e57ea21ec8af3f5e
SimoCrisce <126293808+SimoCrisce@users.noreply.github.com>
2024-03-20 17:52:57 +0100

    adds movie card

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
 
 const Details = function () {
   const [movieObj, setMovieObj] = useState(null);
@@ -7,18 +9,37 @@ const Details = function () {
   const params = useParams();
   const navigate = useNavigate();
 
-  fetch("http://www.omdbapi.com/?apikey=ba8eda7c&i=" + params.movieId)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Errore nel reperimento dei dati richiesti");
-      }
-    })
-    .then()
-    .catch((error) => console.log(error));
-
-  useEffect(() => {}, []);
+  useEffect(() => {
+    fetch("http://www.omdbapi.com/?apikey=ba8eda7c&i=" + params.movieId)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Errore nel reperimento dei dati richiesti");
+        }
+      })
+      .then((movie) => {
+        setMovieObj(movie);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <Container className="mt-3" style={{ width: "400px" }}>
+      {movieObj && (
+        <Card>
+          <Card.Img variant="top" src={movieObj.Poster} />
+          <Card.Body>
+            <Card.Title>{movieObj.Title}</Card.Title>
+            <Card.Text className="d-flex justify-content-between">
+              <div>Voto: {movieObj.imdbRating} / 10</div>
+              <div>Anno: {movieObj.Year}</div>
+            </Card.Text>
+            <Card.Text>Trama: {movieObj.Plot}</Card.Text>
+          </Card.Body>
+        </Card>
+      )}
+    </Container>
+  );
 };
 
 export default Details;
